32509b9c65c48a7a6f0a5bf805c68f1a11f7ae0f
Sofía Gonzalez <sofiiaglz@hotmail.com>
2023-02-09 18:36:49 -0600

    first commit buddy

---
+++ Prueba2Practica1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prueba2Practica1
+//
+//  Created by Mac19 on 07/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
